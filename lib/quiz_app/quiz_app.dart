94398bf891a4cf25e9c180d1cb4d3b14051fa3ce
mynksnh <ms070583@outlook.com>
2022-10-09 23:02:18 +0530

    section 2 anonymous functions

--- lib/quiz_app/quiz_app.dart
+++ lib/quiz_app/quiz_app.dart
@@ -1,14 +1,40 @@
 import 'package:flutter/material.dart';
 
 class QuizApp extends StatelessWidget {
+  void answerQuestion() {
+    print('Answer chosen!');
+  }
+
   @override
   Widget build(BuildContext context) {
+    var questions = [
+      'What\'s your favorite color?',
+      'What\'s your favorite animarl?',
+    ];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: const Text('My First App'),
         ),
-        body: const Text('This is my default text!'),
+        body: Column(
+          children: [
+            const Text('The question!'),
+            ElevatedButton(
+              onPressed: answerQuestion,
+              child: const Text('Answer 1'),
+            ),
+            ElevatedButton(
+              onPressed: () => print('Answer 2 chosen!'),
+              child: const Text('Answer 2'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                print('Answer 3 chosen');
+              },
+              child: const Text('Answer 3'),
+            ),
+          ],
+        ),
       ),
     );
   }
