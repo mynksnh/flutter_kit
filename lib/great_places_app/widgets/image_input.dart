303a8bc567cfd4c941f57858c92a36a8883106c5
mynksnh <ms070583@outlook.com>
2022-10-16 23:40:35 +0530

    Section 13 image picker

---
+++ lib/great_places_app/widgets/image_input.dart
@@ -0,0 +1,65 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:path/path.dart' as path;
+import 'package:path_provider/path_provider.dart' as syspaths;
+
+class ImageInput extends StatefulWidget {
+  @override
+  _ImageInputState createState() => _ImageInputState();
+}
+
+class _ImageInputState extends State<ImageInput> {
+  var _storedImage;
+
+  Future<void> _takePicture() async {
+    final imageXFile = await ImagePicker().pickImage(
+      source: ImageSource.camera,
+      maxWidth: 600,
+    );
+    setState(() {
+      _storedImage = File(imageXFile?.path ?? "");
+    });
+    var imageFile = File(imageXFile?.path ?? "");
+    final appDir = await syspaths.getApplicationDocumentsDirectory();
+    final fileName = path.basename(imageFile.path);
+    final savedImage = await imageFile.copy('${appDir.path}/$fileName');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: <Widget>[
+        Container(
+          width: 150,
+          height: 100,
+          decoration: BoxDecoration(
+            border: Border.all(width: 1, color: Colors.grey),
+          ),
+          alignment: Alignment.center,
+          child: _storedImage != null
+              ? Image.file(
+                  _storedImage,
+                  fit: BoxFit.cover,
+                  width: double.infinity,
+                )
+              : const Text(
+                  'No Image Taken',
+                  textAlign: TextAlign.center,
+                ),
+        ),
+        const SizedBox(
+          width: 10,
+        ),
+        Expanded(
+          child: TextButton.icon(
+            icon: const Icon(Icons.camera),
+            label: const Text('Take Picture'),
+            onPressed: _takePicture,
+          ),
+        ),
+      ],
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -3,12 +3,13 @@ import 'package:flutter/material.dart';
 //import './quiz_app/quiz_app.dart';
 //import './personal-expenses_app/personal_expenses_app.dart';
 //import './deli_meals/deli_meals.dart';
-import './shop_app/shop_app.dart';
+//import './shop_app/shop_app.dart';
+import './great_places_app/great_places_app.dart';
 
 void main() {
   // SystemChrome.setPreferredOrientations([
   //   DeviceOrientation.portraitUp,
   //   DeviceOrientation.portraitUp,
   // ]);
-  runApp(ShopApp());
+  runApp(GreatPlacesApp());
 }
