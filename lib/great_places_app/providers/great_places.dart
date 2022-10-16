0eb78e966b43f431f85a8c59e7f4adcb1c1ce280
mynksnh <ms070583@outlook.com>
2022-10-17 01:36:23 +0530

    Section 13 sqflite

--- lib/great_places_app/providers/great_places.dart
+++ lib/great_places_app/providers/great_places.dart
@@ -1,6 +1,9 @@
+import 'dart:io';
+
 import 'package:flutter/foundation.dart';
 
 import '../models/place.dart';
+import '../helpers/db_helper.dart';
 
 class GreatPlaces with ChangeNotifier {
   List<Place> _items = [];
@@ -11,7 +14,7 @@ class GreatPlaces with ChangeNotifier {
 
   void addPlace(
     String pickedTitle,
-    var pickedImage,
+    File pickedImage,
   ) {
     final newPlace = Place(
       id: DateTime.now().toString(),
@@ -20,6 +23,26 @@ class GreatPlaces with ChangeNotifier {
       location: PlaceLocation(latitude: 0, longitude: 0, address: ""),
     );
     _items.add(newPlace);
+    notifyListeners();
+    DBHelper.insert('user_places', {
+      'id': newPlace.id,
+      'title': newPlace.title,
+      'image': newPlace.image.path,
+    });
+  }
+
+  Future<void> fetchAndSetPlaces() async {
+    final dataList = await DBHelper.getData('user_places');
+    _items = dataList
+        .map(
+          (item) => Place(
+            id: item['id'],
+            title: item['title'],
+            image: File(item['image']),
+            location: PlaceLocation(latitude: 0, longitude: 0, address: ""),
+          ),
+        )
+        .toList();
     notifyListeners();
   }
 }
