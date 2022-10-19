062d352d894006c43e3fc71356ed15893165228b
mynksnh <ms070583@outlook.com>
2022-10-19 23:57:20 +0530

    Restructure Folders

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
 
-//import './quiz_app/quiz_app.dart';
-//import './personal-expenses_app/personal_expenses_app.dart';
-//import './deli_meals/deli_meals.dart';
-//import './shop_app/shop_app.dart';
-//import './great_places_app/great_places_app.dart';
-//import './misc/basic_screen.dart';
-import './misc/immutable_widget.dart';
+import './academindFlutterApps/quiz_app/quiz_app.dart';
+import './academindFlutterApps/personal-expenses_app/personal_expenses_app.dart';
+import './academindFlutterApps/deli_meals/deli_meals.dart';
+import './academindFlutterApps/shop_app/shop_app.dart';
+import './academindFlutterApps/great_places_app/great_places_app.dart';
+import './flutterCookbook/Ch03WidgetsIntro/basic_screen.dart';
+import '../flutterCookbook/Ch03WidgetsIntro/immutable_widget.dart';
 
 void main() {
   // SystemChrome.setPreferredOrientations([
